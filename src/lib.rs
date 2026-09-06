efc950df77fc839d42d9221b5ba06ddb08f6b44e
Ilian Nilsson <ilian.nilsson@live.se>
2026-09-06 08:28:35 +0200

    Consolidate 8 crates onto core declare_error!/declare_retryable_error! (ADR-0037)

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,9 @@
 #![forbid(unsafe_code)]
 
 use message::Message;
-use std::error::Error;
-use std::fmt;
 use xcore::PartyId;
 
-#[derive(Debug)]
-pub struct ProcessError {
-    pub retryable: bool,
-    pub message: String,
-}
-
-impl fmt::Display for ProcessError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.message)
-    }
-}
-impl Error for ProcessError {}
+xcore::declare_retryable_error!(ProcessError);
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ProcessOutcome {
